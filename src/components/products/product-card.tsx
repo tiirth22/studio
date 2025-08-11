4ac353b44ddd792f2137ffda9f3f6735edc70d47
Tirth Dalal <tirth2216@gmail.com>
2025-08-11 08:16:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/products/product-card.tsx
+++ src/components/products/product-card.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
